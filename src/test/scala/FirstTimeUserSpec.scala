e0124f0cfc0126f8fa5c17872ad9bf88362661f3
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2016-12-26 21:48:21 +0000

    some page objects... not quite there yet

--- src/test/scala/FirstTimeUserSpec.scala
+++ src/test/scala/FirstTimeUserSpec.scala
@@ -1,17 +1,17 @@
 import org.scalatest.FlatSpec
 
-class FirstTimeUserSpec extends FlatSpec with FuelMeterAcceptanceTest {
+class FirstTimeUserSpec extends FuelMeterAcceptanceTest {
 
   behavior of "Homepage"
 
   it should "have the correct title" in {
-    go to baseUrl
+    go to homePage
     pageTitle shouldBe "Fuel Meter"
   }
 
-  it should "list vehicle readings if cookie present" in {
+  it should "list vehicle readings if cookie is present" in {
     add cookie("vreg", defaultReg)
-    go to baseUrl
+    go to homePage
     find(tagName("h1")) textShouldBe defaultReg
   }
 
--- src/test/scala/FuelMeterAcceptanceTest.scala
+++ src/test/scala/FuelMeterAcceptanceTest.scala
@@ -1,9 +1,11 @@
-import org.scalatest.Matchers
-import org.scalatest.selenium.Chrome
+import org.scalatest.selenium.{Chrome, Page}
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FlatSpec, Matchers}
 
-trait FuelMeterAcceptanceTest extends Matchers with Chrome {
+trait FuelMeterAcceptanceTest extends FlatSpec with BeforeAndAfterAll with BeforeAndAfterEach with Matchers with Chrome {
 
-  lazy val baseUrl: String = System.getProperty("fuel-meter.base.url", "http://localhost:9000")
+  implicit lazy val baseUrl: String = System.getProperty("fuel-meter.base.url", "http://localhost:9000")
+
+  lazy val homePage = new HomePage(baseUrl)
 
   val defaultReg = "TE5TR3G"
 
@@ -11,4 +13,16 @@ trait FuelMeterAcceptanceTest extends Matchers with Chrome {
     def textShouldBe(expectedString: String): Unit = el.map(_.text) shouldBe Some(expectedString)
   }
 
+  override protected def afterAll(): Unit = quit()
+
+  def fillForm(id: String)(fields: (String, Any)*): Unit = {
+    fields foreach {
+      case (name, textValue) =>
+        find(cssSelector(s"form#$id input[name=$name]")).foreach(el =>
+          el.asInstanceOf[TextField].value = textValue.toString
+        )
+    }
+  }
 }
+
+class HomePage(val url: String) extends Page {}
