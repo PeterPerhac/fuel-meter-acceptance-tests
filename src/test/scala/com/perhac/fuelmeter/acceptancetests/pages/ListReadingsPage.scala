12e03c59190f43b7022e57fc6db7a78c39c60405
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2016-12-28 21:54:20 +0000

    test adding a new vehicle by typing in the text input and clicking the link

--- src/test/scala/com/perhac/fuelmeter/acceptancetests/pages/ListReadingsPage.scala
+++ src/test/scala/com/perhac/fuelmeter/acceptancetests/pages/ListReadingsPage.scala
@@ -5,9 +5,16 @@ import org.openqa.selenium.WebDriver
 
 class ListReadingsPage(val reg: String)(implicit val baseUrl: BaseUrl, driver: WebDriver) extends FuelMeterPage {
 
+  def addNewVehicle(newVehicleReg: String): Unit = {
+    textField(id("new-vehicle-input")).value = newVehicleReg
+    click on cssSelector("#new-vehicle-input + a")
+  }
+
   def clickAddReading(): Unit = click on id("btn-add-reading")
 
   def itemsInList: Int = findAll(cssSelector("#readings-list li")).size
+
+  def vehicleList(): Seq[String] = findAll(cssSelector("#vehicle-list li")).drop(1).map(_.text takeWhile (_.isLetterOrDigit)).toSeq
 
   override val url: String = baseUrl + s"/vehicles/$reg/readings/list"
 }
---
+++ src/test/scala/com/perhac/fuelmeter/acceptancetests/specs/AddVehicleSpec.scala
@@ -0,0 +1,34 @@
+package com.perhac.fuelmeter.acceptancetests.specs
+
+import com.perhac.fuelmeter.acceptancetests.pages.{AddReadingPage, DeleteReadingsPage, ListReadingsPage}
+import org.scalatest.FlatSpec
+
+class AddVehicleSpec extends FlatSpec with FuelMeterAcceptanceTest {
+
+  val newVehicleReg = "NOOV3H"
+
+  val listReadingsPage = new ListReadingsPage(defaultReg)
+  val addReadingPage = new AddReadingPage(defaultReg)
+
+  override def beforeEach(): Unit = {
+    logger.warn(s"clearing all readings for $defaultReg vehicle")
+    go to new DeleteReadingsPage(newVehicleReg)
+    go to listReadingsPage
+    listReadingsPage.itemsInList shouldBe 0
+    listReadingsPage.vehicleList() should not be empty
+  }
+
+  behavior of "Creating a new vehicle"
+
+  it should "save a new vehicle if registration number is of acceptable format" in {
+    listReadingsPage.addNewVehicle(newVehicleReg)
+
+    addReadingPage.prefillForm()
+    addReadingPage.submit()
+
+    assertNoErrorsOnPage()
+    listReadingsPage.itemsInList shouldBe 1
+    listReadingsPage.vehicleList() should contain(newVehicleReg)
+  }
+
+}
