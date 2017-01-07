fae9a30db70eae881f39a85bd4ffbecced5a8c1f
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-01-07 10:55:51 +0000

    noticed a nested foreach and turned it into for comprehension (saved 40 chars!)

--- src/test/scala/com/perhac/fuelmeter/acceptancetests/pages/FuelMeterPage.scala
+++ src/test/scala/com/perhac/fuelmeter/acceptancetests/pages/FuelMeterPage.scala
@@ -5,12 +5,9 @@ import org.scalatest.selenium.{Page, WebBrowser}
 
 abstract class FuelMeterPage(implicit driver: WebDriver) extends Page with WebBrowser {
 
-  protected def fillForm(id: String)(fields: (String, Any)*): Unit = {
-    fields foreach {
-      case (name, textValue) => find(cssSelector(s"form#$id input[name=$name]")).foreach {
-        _.asInstanceOf[TextField].value = textValue.toString
-      }
-    }
-  }
+  protected def fillForm(id: String)(fields: (String, Any)*): Unit = for {
+    (name, any) <- fields
+    e <- find(cssSelector(s"form#$id input[name=$name]"))
+  } e.asInstanceOf[TextField].value = any.toString
 
 }
